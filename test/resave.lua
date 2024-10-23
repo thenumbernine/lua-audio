22eaeeea5c634f9448494bad5e188826391694e5
Christopher Moore <christopher.e.moore@gmail.com>
2024-10-23 13:17:24 +0600

    more

--- test/resave.lua
+++ test/resave.lua
@@ -3,5 +3,8 @@
 local loader = require 'audio.io.wav'()
 local fn = ...
 local args = loader:load(fn)
+for k,v in pairs(args) do
+	if k ~= 'data' then print(k,v) end
+end
 args.filename = 'resave.wav'
 loader:save(args)
